a434a51afbc998ba0189d50ce22f9abfcd5bf2d0
Nikita <alarmistgg@gmail.com>
2020-11-18 08:36:26 +0300

    Добавление элементов на форму, описание кнопки

--- FunctionGraph/Form1.Designer.cs
+++ FunctionGraph/Form1.Designer.cs
@@ -28,13 +28,177 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.labelErorr1 = new System.Windows.Forms.Label();
+            this.labelErorr2 = new System.Windows.Forms.Label();
+            this.labelErorr3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(220, 12);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 2;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Color = System.Drawing.Color.Green;
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(568, 300);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            title1.Name = "Title1";
+            title1.Text = "График функции y= sin(x)";
+            this.chart1.Titles.Add(title1);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(391, 346);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(271, 58);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Построить график функции";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(12, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(182, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "X0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "XN";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(12, 77);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(182, 26);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(12, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "DX";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(12, 125);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(182, 26);
+            this.textBox3.TabIndex = 7;
+            // 
+            // labelErorr1
+            // 
+            this.labelErorr1.AutoSize = true;
+            this.labelErorr1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelErorr1.ForeColor = System.Drawing.Color.Red;
+            this.labelErorr1.Location = new System.Drawing.Point(41, 10);
+            this.labelErorr1.Name = "labelErorr1";
+            this.labelErorr1.Size = new System.Drawing.Size(37, 16);
+            this.labelErorr1.TabIndex = 8;
+            this.labelErorr1.Text = "Erorr";
+            // 
+            // labelErorr2
+            // 
+            this.labelErorr2.AutoSize = true;
+            this.labelErorr2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelErorr2.ForeColor = System.Drawing.Color.Red;
+            this.labelErorr2.Location = new System.Drawing.Point(41, 58);
+            this.labelErorr2.Name = "labelErorr2";
+            this.labelErorr2.Size = new System.Drawing.Size(37, 16);
+            this.labelErorr2.TabIndex = 9;
+            this.labelErorr2.Text = "Erorr";
+            // 
+            // labelErorr3
+            // 
+            this.labelErorr3.AutoSize = true;
+            this.labelErorr3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelErorr3.ForeColor = System.Drawing.Color.Red;
+            this.labelErorr3.Location = new System.Drawing.Point(41, 106);
+            this.labelErorr3.Name = "labelErorr3";
+            this.labelErorr3.Size = new System.Drawing.Size(37, 16);
+            this.labelErorr3.TabIndex = 10;
+            this.labelErorr3.Text = "Erorr";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelErorr3);
+            this.Controls.Add(this.labelErorr2);
+            this.Controls.Add(this.labelErorr1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label labelErorr1;
+        private System.Windows.Forms.Label labelErorr2;
+        private System.Windows.Forms.Label labelErorr3;
     }
 }
 
--- FunctionGraph/Form1.cs
+++ FunctionGraph/Form1.cs
@@ -15,6 +15,59 @@ namespace FunctionGraph
         public Form1()
         {
             InitializeComponent();
+            label1.Text = "X0";
+            label2.Text = "XN";
+            label3.Text = "DX";
+            AllLeabelErorrClear();
+        }
+
+        private void AllLeabelErorrClear()
+        {
+            labelErorr1.Text = "";
+            labelErorr2.Text = "";
+            labelErorr3.Text = "";
+        }
+
+        public event Action act;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x0 = -10, xn = 10, dx = 0.1, x, y;
+            AllLeabelErorrClear();
+            int flagAct = 0;
+            if (InputErorr(labelErorr1, () => x0 = double.Parse(textBox1.Text)))
+                flagAct++;
+            if (InputErorr(labelErorr2, () => xn = double.Parse(textBox2.Text)))
+                flagAct++;
+            if (InputErorr(labelErorr3, () => dx = double.Parse(textBox3.Text)))
+                flagAct++;
+            if (flagAct == 3)
+            {
+                this.chart1.Series[0].Points.Clear();//Очищаем точки
+                x = x0;
+                while (x<=xn)
+                {
+                    y = Math.Sin(x);
+                    this.chart1.Series[0].Points.AddXY(x, y);
+                    x += dx;
+                }
+            }
+            
+
+        }
+        private bool InputErorr(Label label, Action act)
+        {
+            
+            try
+            {
+                act();
+                return true;
+            }
+            catch (Exception)
+            {
+                label.Text = "Введите число!";
+                return false;
+            }
+
         }
     }
 }
